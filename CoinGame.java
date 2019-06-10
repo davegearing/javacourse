7bc420c00e5f59d3ff19a334b9f361058083657c
DaveGearing <51530523+davegearing@users.noreply.github.com>
2019-06-10 08:53:22 +0100

    coin game
    new program

---
+++ CoinGame.java
@@ -0,0 +1,75 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class CoinGame {
+
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+        set the coin amounts and the total
+         */
+        int oneP = 1;
+        int twoP = 2;
+        int fiveP = 5;
+        int tenP = 10;
+        int twentyP = 20;
+        int fiftyP = 50;
+        int onePnd = 100;
+        int total;
+        int difference;
+
+        /*
+        ask the user how many of each coin type they have
+        pennies, two pence, five pence, ten pence, twenty pence, fifty pence, one pound
+         */
+
+        System.out.println("How many penny coins do you have?");
+        int pennies = scanner.nextInt();
+
+        System.out.println("How many two penny coins do you have?");
+        int tuppence = scanner.nextInt();
+
+        System.out.println("How many five pence coins do you have?");
+        int fivePence = scanner.nextInt();
+
+        System.out.println("How many ten pence coins do you have?");
+        int tenPence = scanner.nextInt();
+
+        System.out.println("How many twenty pence coins do you have?");
+        int twentyPence = scanner.nextInt();
+
+        System.out.println("How many fifty pence coins do you have?");
+        int fiftyPence = scanner.nextInt();
+
+        System.out.println("How many one pound coins do you have?");
+        int onePound = scanner.nextInt();
+
+        scanner.close();
+
+        /*
+        now sum up all the coins they have and show the total
+        show the win/lose if they have a pound or not
+         */
+
+        total = ((pennies * oneP) + (tuppence * twoP) + (fivePence * fiveP)
+                + (tenPence * tenP) + (twentyPence * twentyP)
+                + (fiftyPence * fiftyP) + (onePound * onePnd));
+
+        if(total == 100){
+            System.out.println("You WIN! Total is " + total + " pence");
+        }
+        else{
+            difference = total - 100;
+            if(difference > 0){
+                System.out.println("You LOSE! You are " + difference + " pence over");
+            }
+            else{
+                difference = difference * -1;
+                System.out.println("You LOSE! You are " + difference + " pence under");
+            }
+        }
+
+    }
+}
